1a5e873bd646920ebeead0003780d07382695817
LDan0511 <hanstress0511@gmail.com>
2026-04-15 20:35:54 +0800

    Update train_llf.py

--- train_llf.py
+++ train_llf.py
@@ -1,8 +1,28 @@
-print('我是李凌风')
-print('我是李凌风')
-print('我是李凌风')
-print('我是李凌风')
-print('我是李凌风')
-print('我是李凌风')
-print('我是李凌风')
-#13123213123123
+import torch
+import torch.nn as nn
+
+class SmallDOINet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.body = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(32, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(32, 16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(16, 1, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x):
+        residual = self.body(x)
+        out = x + residual
+        out = torch.clamp(out, min=0.0)
+        return out
